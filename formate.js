043c65bb61719a1b3b682c74af17f46be3685148
lerbon112 <luohaibin@yagena.net>
2021-05-11 18:59:43 +0800

    格式化

---
+++ formate.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const findMdFilePath = (filePath, res) => {
+  const files = fs.readdirSync(filePath);
+  files.forEach(item => {
+    const stat = fs.statSync(path.join(filePath, item));
+    if (stat.isDirectory() && !/\.git/.test(item)) {
+      findMdFilePath(path.join(filePath, item), res);
+    } else {
+      if (item.endsWith('\.md')) {
+        res.push(path.join(filePath, item));
+      }
+    }
+  });
+};
+
+const files = [];
+findMdFilePath(process.cwd(), files);
+
+const addSpace = (files) => {
+  files.forEach((file, i) => {
+    const res = fs.readFileSync(file, 'utf8');
+    const fileStr = res.toString().split('\r').map(item=>item + '  ');
+    fs.writeFileSync(file, fileStr.join(''), 'utf8');
+  });
+
+}
+
+addSpace(files);
